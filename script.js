4dde80d64bdea171f5c4e05f4a909fe1a9c85982
e8269crd <jdhtroy@hendersonclark.com>
2026-02-08 14:50:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Sends you to the JavaScript page
-function goToJS() {
-  // Replace this later with your real JS page link
-  window.location.href = "javascript.html";
+function goToHTML() {
+  window.location.href = "index.html";
 }
 
-// Sends you to the CSS page
 function goToCSS() {
-  // Replace this later with your real CSS page link
   window.location.href = "css.html";
+}
+
+function goToJS() {
+  window.location.href = "js.html";
 }
